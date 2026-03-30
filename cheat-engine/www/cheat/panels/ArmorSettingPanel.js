8530c87c33156acfd09de4aa16f30c00e5e312b2
eduwill <eduwill100@gmail.com>
2026-03-30 19:22:18 -0300

    Fix item description translation, normalize cache keys, fix search crash, and add UI switches for expanded targets.

--- cheat-engine/www/cheat/panels/ArmorSettingPanel.js
+++ cheat-engine/www/cheat/panels/ArmorSettingPanel.js
@@ -1,4 +1,5 @@
 import ItemTableTab from './ItemTableTab.js'
+import {TRANSLATE_SETTINGS, TRANSLATION_BANK} from '../js/TranslateHelper.js'
 
 export default {
     name: 'ArmorSettingPanel',
@@ -38,17 +39,45 @@ export default {
 
     created () {
         this.initializeVariables()
+
+        this._translateListener = () => {
+            if (TRANSLATE_SETTINGS.isArmorTranslateEnabled && TRANSLATE_SETTINGS.isArmorTranslateEnabled()) {
+                this.initializeVariables()
+            }
+        }
+        window.addEventListener('cheat-translate-finish', this._translateListener)
+    },
+
+    beforeDestroy () {
+        if (this._translateListener) {
+            window.removeEventListener('cheat-translate-finish', this._translateListener)
+        }
     },
 
     methods: {
         initializeVariables () {
-            this.items = $dataArmors
+            this.items = $dataArmors.slice()
         },
 
         convertToTableData (item) {
+            let translatedName = item.name
+            let translatedDesc = item.description
+
+            if (TRANSLATE_SETTINGS.getTargets().armors) {
+                if (item.name && item.name.trim()) {
+                    const cachedName = TRANSLATION_BANK.get(item.name)
+                    if (cachedName) translatedName = cachedName.translated
+                }
+                
+                if (item.description && item.description.trim()) {
+                    const cachedDesc = TRANSLATION_BANK.get(item.description)
+                    if (cachedDesc) translatedDesc = cachedDesc.translated
+                }
+            }
+
             return {
-                name: item.name,
-                desc: item.description
+                name: translatedName,
+                desc: translatedDesc
             }
         }
     }
